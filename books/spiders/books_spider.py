10564162db7a9a6b14cf046a6ff5c86098984088
kabust <tulyulyuk91@gmail.com>
2024-04-24 14:32:36 +0200

    added annotations, handled elements parsing to separate class

--- books/spiders/books_spider.py
+++ books/spiders/books_spider.py
@@ -1,8 +1,7 @@
-from pathlib import Path
-
-import requests
 import scrapy
-from scrapy.http import Response, HtmlResponse
+from scrapy.http import Response
+
+from books.spiders.utils import BookScraper
 
 
 class BooksSpider(scrapy.Spider):
@@ -10,11 +9,11 @@ class BooksSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com/catalogue/page-1.html"]
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
         self.base_url = "https://books.toscrape.com/catalogue/"
 
-    def parse(self, response: Response, **kwargs):
+    def parse(self, response: Response, **kwargs) -> scrapy.Request:
         next_page = response.css("li.next > a::attr(href)").get()
         urls = self._get_page_urls(response)
 
@@ -24,29 +23,22 @@ class BooksSpider(scrapy.Spider):
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
 
-    def _get_page_urls(self, response: Response) -> [str]:
+    def _get_page_urls(self, response: Response) -> list[str]:
         return [
             self.base_url + url
             for url in response.css("article > h3 > a::attr(href)").getall()
         ]
 
-    def _scrape_single_book(self, response: Response) -> dict:
-        rating_map = {
-            "One": 1,
-            "Two": 2,
-            "Three": 3,
-            "Four": 4,
-            "Five": 5
-        }
-
-        amount_in_stock = response.css("tr:nth-child(6) > td::text").extract_first().split()[2][1:]
+    @staticmethod
+    def _scrape_single_book(response: Response) -> dict:
+        book_scraper = BookScraper(response)
 
         yield dict(
-            title=response.css(".product_main > h1::text").get(),
-            price=float(response.css(".price_color::text").get()[2:]),
-            amount_in_stock=int(amount_in_stock) if amount_in_stock is not None else 0,
-            rating=int(rating_map[response.css(".star-rating::attr(class)").get().split()[-1]]),
-            category=response.css(".breadcrumb > li:nth-child(3) > a::text").get(),
-            description=response.css("#content_inner > article > p::text").get(),
-            upc=response.css("tr:nth-child(1) > td::text").extract_first()
+            title=book_scraper.get_title(),
+            price=book_scraper.get_price(),
+            amount_in_stock=book_scraper.get_amount_in_stock(),
+            rating=book_scraper.get_rating(),
+            category=book_scraper.get_category(),
+            description=book_scraper.get_description(),
+            upc=book_scraper.get_upc()
         )
---
+++ books/spiders/utils.py
@@ -0,0 +1,42 @@
+from scrapy.http import Response
+
+
+class BookScraper:
+    def __init__(self, response: Response) -> None:
+        self.response = response
+
+    def get_title(self) -> str:
+        item = self.response.css(".product_main > h1::text").get()
+        return item
+
+    def get_price(self) -> float:
+        item = self.response.css(".price_color::text").get()
+        return float(item[2:])
+
+    def get_amount_in_stock(self) -> int:
+        item = self.response.css("tr:nth-child(6) > td::text").extract_first()
+        amount_in_stock = item.split()[2][1:]
+        return int(amount_in_stock) if amount_in_stock is not None else 0
+
+    def get_rating(self) -> int:
+        rating_map = {
+            "One": 1,
+            "Two": 2,
+            "Three": 3,
+            "Four": 4,
+            "Five": 5
+        }
+        item = self.response.css(".star-rating::attr(class)").get()
+        return int(rating_map[item.split()[-1]])
+
+    def get_category(self) -> str:
+        item = self.response.css(".breadcrumb > li:nth-child(3) > a::text").get()
+        return item
+
+    def get_description(self) -> str:
+        item = self.response.css("#content_inner > article > p::text").get()
+        return item
+
+    def get_upc(self) -> str:
+        item = self.response.css("tr:nth-child(1) > td::text").extract_first()
+        return item
